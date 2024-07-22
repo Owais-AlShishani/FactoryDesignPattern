99d16f513e70b27c8ab66e2a65e6d7eab1d9927a
Owais-Alshishani <Awais.alshishani@gmail.com>
2024-07-22 12:17:41 +0300

    Add project files.

---
+++ FactoryDesignPattern/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace FactoryDesignPattern
+{
+    public abstract class Car
+    {
+        public abstract void Assemble();
+    }
+    public class BMW : Car
+    {
+        public override void Assemble()
+        {
+            Console.WriteLine("Assembling BMW Car.");
+        }
+    }
+    public class Lada : Car
+    {
+        public override void Assemble()
+        {
+            Console.WriteLine("Assembling Lada Car.");
+        }
+    }
+
+    public class Toyota : Car
+    {
+        public override void Assemble()
+        {
+            Console.WriteLine("Assembling Toyota Car.");
+        }
+    }
+
+    public class CarFactory
+    {
+        public Car CreateFactory(string carType)
+        {
+            return carType.ToLower() switch
+            {
+                "bmw" => new BMW(),
+                "lada" => new Lada(),
+                "toyota" => new Toyota(),
+                _ => throw new ArgumentException
+                                        ($"Invalid car type: {carType}"),
+            };
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CarFactory carFactory = new();
+
+            carFactory.CreateFactory("BMW").Assemble();
+            carFactory.CreateFactory("Lada").Assemble();
+            carFactory.CreateFactory("Toyota").Assemble();
+        }
+    }
+}
